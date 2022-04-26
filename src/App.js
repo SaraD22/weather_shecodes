3ef29625eb5d4d301979e097a31776fdb4971dfb
SaraD22 <91880957+SaraD22@users.noreply.github.com>
2022-04-26 21:34:32 +0200

    Added boostrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This project was coded by Sara Duarte and is{" "}
-        <a href="https://github.com/SaraD22/weather_shecodes">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          {" "}
+          This project was coded by Sara Duarte and is{" "}
+          <a href="https://github.com/SaraD22/weather_shecodes">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
